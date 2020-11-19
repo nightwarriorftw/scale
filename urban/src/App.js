bd10492b3bb2d5d107e7389a8e10180ade76039a
nightwarriorftw <amanv1999@gmail.com>
2020-11-19 16:21:11 +0530

    Setup routes for frontend

--- urban/src/App.js
+++ urban/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+      </div>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/interviews" component={Interviews} />
+        <Route exact path="/interviews/:id" component={InterviewDetails} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
