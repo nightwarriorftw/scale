fdebccc57bd87e10f632022a53c69cbfd3568f19
nightwarriorftw <amanv1999@gmail.com>
2020-11-22 00:03:42 +0530

    Updated UI

--- urban/src/components/Home.js
+++ urban/src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div style={{ textAlign: "center", marginTop: "100px"}}>
+    <div style={{ textAlign: "center", marginTop: "50px"}}>
       <img src={process.env.PUBLIC_URL + "/scale.png"}  width="100px" heigh="100px" alt='logo'/>
       <h1>Scale</h1>
       <h2>Host and Schedule Interviews</h2>
