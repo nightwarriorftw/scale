65792a47c274de05d7cd7dfd053b2ec17d18aa10
nightwarriorftw <amanv1999@gmail.com>
2020-11-21 21:56:19 +0530

    Added changes of Home component

--- urban/src/components/Home.js
+++ urban/src/components/Home.js
@@ -4,7 +4,6 @@ const Home = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "100px"}}>
       <img src={process.env.PUBLIC_URL + "/scale.png"}  width="100px" heigh="100px" alt='logo'/>
-      <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
       <h1>Scale</h1>
       <h2>Host and Schedule Interviews</h2>
     </div>
